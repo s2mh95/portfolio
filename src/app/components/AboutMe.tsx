c5ab67e39e976a99dabaab24c51f594bb3490004
Meysam Mirhosseini <meysammirhossini@gmail.com>
2024-07-20 12:29:01 +0330

    commit

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -7,7 +7,11 @@ export default function AboutMe() {
       <div id="about" className="flex flex-col lg:flex-row lg:p-32 my-10 lg:my-20 text-white">
         <div className="w-full lg:w-1/2 p-6 ">
           <h1 className="text-3xl lg:text-5xl font-bold mb-6 lg:mb-10">About <strong className="text-[#00ADB5]">Me</strong></h1>
-          <p className="text-lg lg:text-2xl w-full lg:w-5/6 m-auto lg:font-semibold">Hello! I'm Seyed Meysam Mir Hosseini, an electrical engineer with a passion for coding and technology. With a strong foundation in electrical engineering, I've seamlessly integrated my technical skills into the world of software development. Over the past few years, I've delved deep into the realm of programming, specializing in Python development and backend technologies like Django.</p>
+          <p className="text-lg lg:text-2xl w-full lg:w-5/6 m-auto lg:font-semibold">
+          Electrical Engineer | Fullstack Developer | Frontend Specialist <br />
+<br />
+          As an electrical engineer with a deep-seated passion for coding and technology, I have effectively merged my engineering expertise with advanced software development skills. My strong foundation in electrical engineering has seamlessly transitioned into the realm of programming, where I specialize in Python and React development and Fullstack technologies. Over the past few years, I have honed my abilities in these areas, leveraging my technical background to build robust and efficient solutions.
+          </p>
         </div>
         <div className="relative w-full lg:w-1/2 bg-[url('../../public/pic2.png')] bg-cover bg-center min-h-[550px]">
           <Image className="m-auto" src="/man.png" alt="" width={500} height={100}/>
