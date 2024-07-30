2dee95ffc2f8f841db560574cfc50e7f8e26edb9
Meysam Mirhosseini <meysammirhossini@gmail.com>
2024-07-30 10:47:49 +0330

    commit

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -3,6 +3,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Projects() {
     const settings = {
@@ -44,52 +45,28 @@ export default function Projects() {
         <div id='projects' className="slider-container items-center text-center py-10 my-20 skills bg-[url('../../public/pic2.png')] mx-10 h-[600px] text-white">
             <h1 className='text-5xl font-bold'>Project</h1>
         <Slider {...settings}>
-            <div className="flex justify-between items-center">
-                <div className="flex flex-col font-bold text-4xl rounded-3xl m-10 pb-5 bg-gray-400 opacity-60 hover:opacity-80">
-                    <Image className="m-auto" src="/p.png" alt="mypic" width={300} height={100} />
-                    Project 1
+            <Link href="https://lucent-panda-03a078.netlify.app/" className="flex justify-between items-center">
+                <div className="flex flex-col font-bold text-4xl rounded-3xl m-10 pb-5 h-96 bg-gray-400 opacity-60 hover:opacity-80">
+                    <Image className="m-auto " src="/ecommerce.png" alt="mypic" width={300} height={100} />
+                    eCommerce
                 </div>
-            </div>
-            <div className="flex justify-between items-center m-auto">
-                <div className="flex flex-col font-bold text-4xl rounded-3xl m-10 pb-5 bg-gray-400 opacity-60 hover:opacity-80">
-                    <Image className="m-auto" src="/p.png" alt="mypic" width={300} height={100} />
-                    Project 2
+            </Link>
+            <Link href="https://spectacular-heliotrope-31b851.netlify.app/" className="flex justify-between items-center m-auto">
+                <div className="flex flex-col font-bold text-4xl rounded-3xl m-10 pb-5 h-96 bg-gray-400 opacity-60 hover:opacity-80">
+                    <Image className="m-auto" src="/duolingo.png" alt="mypic" width={300} height={100} />
+                    Duolingo clone
                 </div>
-            </div>
+            </Link>
             <div className="flex justify-between items-center m-auto">
-                <div className="flex flex-col font-bold text-4xl rounded-3xl m-10 pb-5 bg-gray-400 opacity-60 hover:opacity-80">
+                <div className="flex flex-col font-bold text-4xl rounded-3xl m-10 pb-5 h-96 bg-gray-400 opacity-60 hover:opacity-80">
                     <Image className="m-auto" src="/p.png" alt="mypic" width={300} height={100} />
                     Project 3
                 </div>
             </div>
             <div className="flex justify-between items-center m-auto">
-                <div className="flex flex-col font-bold text-4xl rounded-3xl m-10 pb-5 bg-gray-400 opacity-60 hover:opacity-80">
+                <div className="flex flex-col font-bold text-4xl rounded-3xl m-10 pb-5 h-96 bg-gray-400 opacity-60 hover:opacity-80">
                     <Image className="m-auto" src="/p.png" alt="mypic" width={300} height={100} />
                     Project 4
-                </div>
-            </div>
-            <div className="flex justify-between items-center m-auto">
-                <div className="flex flex-col font-bold text-4xl rounded-3xl m-10 pb-5 bg-gray-400 opacity-60 hover:opacity-80">
-                    <Image className="m-auto" src="/p.png" alt="mypic" width={300} height={100} />
-                    Project 5
-                </div>
-            </div>
-            <div className="flex justify-between items-center m-auto">
-                <div className="flex flex-col font-bold text-4xl rounded-3xl m-10 pb-5 bg-gray-400 opacity-60 hover:opacity-80">
-                    <Image className="m-auto" src="/p.png" alt="mypic" width={300} height={100} />
-                    Project 6
-                </div>
-            </div>
-            <div className="flex justify-between items-center m-auto">
-                <div className="flex flex-col font-bold text-4xl rounded-3xl m-10 pb-5 bg-gray-400 opacity-60 hover:opacity-80">
-                    <Image className="m-auto" src="/p.png" alt="mypic" width={300} height={100} />
-                    Project 7
-                </div>
-            </div>
-            <div className="flex justify-between items-center m-auto">
-                <div className="flex flex-col font-bold text-4xl rounded-3xl m-10 pb-5 bg-gray-400 opacity-60 hover:opacity-80">
-                    <Image className="m-auto" src="/p.png" alt="mypic" width={300} height={100} />
-                    Project 8
                 </div>
             </div>
         </Slider>
